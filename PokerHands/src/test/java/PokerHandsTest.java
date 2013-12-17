068f2101e91f3db939fe56a37c125c206ce7884c
Blackbaud-JohnTurner <john.turner@blackbaud.com>
2013-12-17 10:20:31 -0600

    added poker hands project

---
+++ PokerHands/src/test/java/PokerHandsTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class PokerHandsTest {
+
+    @Test
+    public void testImport() {
+        assertTrue(true);
+    }
+}
